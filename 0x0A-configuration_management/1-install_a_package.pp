2c00a41d74ad9b043900ce2a8353fcd9b756b4b0
Kroser-draid <kingdomskroser@gmail.com>
2024-07-03 17:02:56 -0700

    fix 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,10 @@ package { 'flask' :
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# ensure the Werkzeug package installed
+
+package { 'Werkzeug' :
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
